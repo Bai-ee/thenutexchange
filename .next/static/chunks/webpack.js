d756ba4881188b2fd8f2e1cbc9c0037ee643960c
Bryan  Balli <bryanballi@bryans-mac-mini.lan>
2024-01-17 11:25:19 -0600

    wip

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7614a7274bba45be"; }
+/******/ 		__webpack_require__.h = function() { return "a1cd2f7a06c08131"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.624ec5c60707539d.hot-update.js
+++ .next/static/webpack/webpack.624ec5c60707539d.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "b053ea6608888b66"; }
+/******/ 	__webpack_require__.h = function() { return "a1cd2f7a06c08131"; }
 /******/ }();
 /******/ 
 /******/ }
